4838cbf25407c693b49e70c74006d28e8edbc7c2
ArmanceL <93270569+ArmanceL@users.noreply.github.com>
2022-05-03 13:06:05 +0200

    Add files via upload

---
+++ Scripts/setup.R
@@ -0,0 +1,8 @@
+#############################################
+## The following loads the needed packages ##
+#############################################
+
+# load the required packages
+packages <- c()
+
+purrr::walk(packages, library, character.only = TRUE)
